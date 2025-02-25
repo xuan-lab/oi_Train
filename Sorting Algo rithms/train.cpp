fc852f4a3eb1b1cb492ae203e73bcef342c9c32d
xuan-lab <xuan1024chem@gmail.com>
2025-02-26 00:03:32 +0800

    first day

--- Sorting Algo rithms/train.cpp
+++ Sorting Algo rithms/train.cpp
@@ -14,16 +14,38 @@ void budding_sort(int arr[],int len){
         }
     }
 }
+void selection_sort(int arr[],int len){
+    for(int i=0;i<len-1;i++){
+        int min_index=i;
+        for(int j=i+1;j<len;j++){
+            if(arr[j]<arr[min_index]){
+                min_index=j;
+            }
+        }
+        swap(arr[i],arr[min_index]);
+    }
+}
 
+void insertion_sort(int arr[],int len){
+    for(int i=1;i<len;i++){
+        int key=arr[i];
+        int j=i-1;
+        while(j>=0 && arr[j]>key){
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=key;
+    }
+}
 int main()
 {
     int arr[]={1,7,5,3,9,4};
     budding_sort(arr,6);
+    //selection_sort(arr,6);
+    //insertion_sort(arr,6);
     for(int i = 0; i < 6; i++)
     {
         cout<<arr[i]<<" ";
     }
     return 0;
 }
-// Time Complexity: O(n^2)
-// Space Complexity: O(1)  
